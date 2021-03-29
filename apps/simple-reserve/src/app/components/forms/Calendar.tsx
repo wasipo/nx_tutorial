d197a0a77a0f1eb5c5faf708c92a3dddcfa4d2ca
wasipo <horiguchiko@gmail.com>
2021-03-29 23:35:02 +0900

    Weekly日付フォーマット日本語化に成功

--- apps/simple-reserve/src/app/components/forms/Calendar.tsx
+++ apps/simple-reserve/src/app/components/forms/Calendar.tsx
@@ -9,57 +9,73 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import {AppointmentForm, FormatterFn, ViewState} from '@devexpress/dx-react-scheduler';
 import moment from 'moment';
-import { withStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
 import { useCallback } from 'react';
+import { ModeCommentTwoTone } from '@material-ui/icons';
 
 moment.locale('ja');
+const d = moment().format('YYYY-MM-DD');
+const t = moment().add(7,'days').format('YYYY-MM-DD');
 
-
-
-const formatDayScaleDate = (date, options) => {
-  const momentDate = moment(date);
-  const { weekday } = options;
-
-  return momentDate.format(weekday ? 'dddd' : 'D');
-};
 
 const formatTimeScaleDate = date => moment(date).format('hh:mm');
 
-const styles = {
-  dayScaleCell: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  },
-};
 
-// const DayScaleCell = withStyles(styles, 'DayScaleCell')((
-//   { formatDate, classes, ...restProps }
-//   ,) => (
-//       <WeekView.DayScaleCell
-//         {...restProps}
-//         formatDate={formatDayScaleDate}
-//         className={classes.dayScaleCell}
-//       />
-// ));
+const useStyles = makeStyles(theme => ({
+  todayCell: {
+    backgroundColor: fade(theme.palette.primary.main, 0.1),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.primary.main, 0.14),
+    },
+    '&:focus': {
+      backgroundColor: fade(theme.palette.primary.main, 0.16),
+    },
+  },
+  weekendCell: {
+    backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    },
+    '&:focus': {
+      backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+    },
+  },
+  today: {
+    backgroundColor: fade(theme.palette.primary.main, 0.16),
+  },
+  weekend: {
+    backgroundColor: fade(theme.palette.action.disabledBackground, 0.06),
+  },
+}));
+
+
 
 const TimeScaleLabel = (
   { formatDate, ...restProps },
 ) => <WeekView.TimeScaleLabel {...restProps} formatDate={formatTimeScaleDate} />;
 
-const d = moment().format('YYYY-MM-DD');
-const t = moment().add(7,'days').format('YYYY-MM-DD');
 
-const DayScaleCell = (
-  {formatDate,d,t,...restProps },
-  ) => (
-      <WeekView.DayScaleCell
-        {
-          ...restProps
-        }
-        startDate={d}
-        formatDate={formatDayScaleDate}
-      />
-);
+const formatDayScaleDate = (date, options) => {
+  const momentDate = moment(date);
+  const { weekday } = options;
+  return momentDate.format(weekday ? 'dddd' : 'D');
+};
+
+const DayScaleCell = (props) => {
+
+    const { startDate, today} = props;
+    const classes = useStyles();
+
+    if(today) {
+      return <WeekView.DayScaleCell {...props} formatDate={formatDayScaleDate} className={classes.today} />;
+    }
+    if (startDate.getDay() === 0 || startDate.getDay() === 6) {
+      return <WeekView.DayScaleCell {...props} formatDate={formatDayScaleDate} className={classes.weekend} />;
+    }
+    return (<WeekView.DayScaleCell {...props} className="test" formatDate={formatDayScaleDate}
+    />)
+};
+
 
 const Calendar : React.FC = () => {
 
@@ -70,7 +86,6 @@ const Calendar : React.FC = () => {
 
   const Root = useCallback(
       (props) => {
-        console.log(currentDate);
         return <Toolbar.Root {...props} style={{ background: "#eee" }} />;
       },
       [currentDate]
@@ -88,8 +103,8 @@ const Calendar : React.FC = () => {
           />
           <WeekView
             startDayHour={9}
-            endDayHour={14} 
-            excludedDays={[0,1]}
+            endDayHour={19} 
+            // excludedDays={[0,1]}
             dayScaleCellComponent={DayScaleCell}
             timeScaleLabelComponent={TimeScaleLabel}
           />
